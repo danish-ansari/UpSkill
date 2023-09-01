5347645c994413b22e506f2fcdf040db19a044cd
Danish Ansari <danish.ansari@innovect.com>
2023-09-01 10:56:12 +0530

    Initial Commit

---
+++ UpSkill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpSkill
+//
+//  Created by danish on 01/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
